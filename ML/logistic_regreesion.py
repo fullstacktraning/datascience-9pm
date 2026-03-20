6ccc2a73248b5b17bab7dbfa5a824e1b0ca0aa2c
VPRO <samba.vpro@gmail.com>
2026-03-20 21:49:28 +0530

    code

--- ML/logistic_regreesion.py
+++ ML/logistic_regreesion.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.linear_model import LogisticRegression
 hours = np.array([1,2,3,4,5,6]).reshape(-1,1)
 result = np.array([0,0,0,1,1,1])
-model = LogisticRegression()
+model = LogisticRegression() # sigmoid formula
 
 model.fit(hours,result)
 prediction = model.predict([[3.5]])
