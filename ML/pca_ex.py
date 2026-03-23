29d950dc546eb61cd002d3878c21d63443657088
VPRO <samba.vpro@gmail.com>
2026-03-23 22:25:45 +0530

    pca

--- ML/pca_ex.py
+++ ML/pca_ex.py
@@ -1,10 +1,10 @@
 import numpy as np
 from sklearn.decomposition import PCA
-X = np.array([[2,4,6],
-              [3,6,9],
-              [4,8,12],
-              [5,10,15]])
-model = PCA(n_components=2)
+X = np.array([[4,11],
+              [8,4],
+              [13,5],
+              [7,14]])
+model = PCA(n_components=1)
 X_new = model.fit_transform(X)
 print(X_new)
 
